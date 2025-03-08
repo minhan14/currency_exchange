c6e9f924bff6f19ab26724be8a03b450c11faff7
Chico Han <minhanacc@gmail.com>
2025-03-08 15:50:34 +0630

    update api key and work-flow

--- app/src/main/java/com/chicohan/currencyexchange/helper/Constants.kt
+++ app/src/main/java/com/chicohan/currencyexchange/helper/Constants.kt
@@ -2,5 +2,7 @@ package com.chicohan.currencyexchange.helper
 
 object Constants {
     const val BASE_URL = "https://apilayer.net/api/"
-    const val API_KEY = "bc4be9c6f3fe6f5da8f08e25eda6e929"
+    //    const val API_KEY = "bc4be9c6f3fe6f5da8f08e25eda6e929"
+    const val API_KEY = "eff9c28f4552397223821d3a04cef823"
+
 }
